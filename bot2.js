2951c9178b5f9bd821b5e37cf2894026753ecfbe
Joshua Cox <joshdcox50@gmail.com>
2019-07-30 13:36:46 -0400

    update to bot

---
+++ bot2.js
@@ -0,0 +1,22 @@
+var twit = require("twit");
+
+
+var Bot = new twit({
+    consumer_key: ,
+    consumer_secret: ,
+    access_token: ,
+    access_token_secret: ,
+});
+
+var users = ["50323173", "178580925"];
+
+var stream = Bot.stream('statuses/filter', { follow: users });
+
+stream.on('tweet', function(tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        Bot.post('statuses/retweet/:id', { id: tweet.id_str }, function(err, data, response) {
+            console.log(data)
+        })
+    }
+})
