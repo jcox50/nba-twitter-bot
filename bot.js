d9eca936d379651570b389b614ee4a63d642584b
Joshua Cox <joshdcox50@gmail.com>
2019-07-30 20:57:11 -0400

    updated user list of users to monitor

--- bot.js
+++ bot.js
@@ -1,17 +1,20 @@
 var Twit = require('twit');
-var TwitterBot = require('node-twitterbot').TwitterBot;
+
+
 require('dotenv').config()
 
-var Bot = new TwitterBot({
+var Bot = new Twit({
     consumer_key: process.env.BOT_CONSUMER_KEY,
     consumer_secret: process.env.BOT_CONSUMER_SECRET,
     access_token: process.env.BOT_ACCESS_TOKEN,
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
-var users = ["50323173", "178580925"];
+var users = ["50323173", "178580925", "17292143", "2352759108", "193095044", "48488561"];
 
 var stream = Bot.stream('statuses/filter', { follow: users });
+
+console.log("reached here")
 
 stream.on('tweet', function(tweet) {
     if (users.indexOf(tweet.user.id_str) > -1) {
@@ -21,3 +24,5 @@ stream.on('tweet', function(tweet) {
         })
     }
 })
+
+console.log("reached here after stream code")
--- bot2.js
+++ bot2.js
@@ -1,11 +1,11 @@
 var twit = require("twit");
-
+var TwitterBot = require('node-twitterbot').TwitterBot;
 
 var Bot = new twit({
-    consumer_key: ,
-    consumer_secret: ,
-    access_token: ,
-    access_token_secret: ,
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
 
