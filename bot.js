d505e67b2e1e8ab2e1bd2e09d4543d986dbcb8da
Joshua Cox <joshdcox50@gmail.com>
2019-07-30 20:11:10 -0400

    updated bot file

--- bot.js
+++ bot.js
@@ -8,20 +8,16 @@ var Bot = new TwitterBot({
     access_token: process.env.BOT_ACCESS_TOKEN,
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
-var phraseArray = ["hey twitter",
-    "im tweeting",
-    "tweet tweet",
-    "tweetstorm time... 1/22",
-    "plz RT v important",
-    "delete ur account",
-    "it me",
-    "same",
-    "#dogpants go on 4 legs!!",
-    "#thedress is blue and black"
-];
 
-function chooseRandom(myArray) {
-    return myArray[Math.floor(Math.random() * myArray.length)];
-}
-var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
-Bot.tweet(phrase);
+var users = ["50323173", "178580925"];
+
+var stream = Bot.stream('statuses/filter', { follow: users });
+
+stream.on('tweet', function(tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        Bot.post('statuses/retweet/:id', { id: tweet.id_str }, function(err, data, response) {
+            console.log(data)
+        })
+    }
+})
